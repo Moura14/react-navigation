2f5ab54cc567050fe309eb42369e4820039666dd
João Gabriel <87152738+Moura14@users.noreply.github.com>
2025-05-17 12:58:42 -0300

    feat: preparando o APp

---
+++ app/index.jsx
@@ -0,0 +1,8 @@
+import { SafeAreaView } from 'react-native'
+import TextoCentral from './(tabs)/componentes/TextoCentral'
+
+export default props => (
+    <SafeAreaView style={{flex: 1}}>
+        <TextoCentral>App</TextoCentral>
+    </SafeAreaView>
+)
